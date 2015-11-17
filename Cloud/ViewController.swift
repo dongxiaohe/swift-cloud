4af7a62da668ecd15199f511b36c46d810806223
Danny Dong <danny.dong@vic017589m.win.int.realestate.com.au>
2015-11-18 10:39:18 +1100

    initial commit

---
+++ Cloud/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Cloud
+//
+//  Created by Danny Dong on 11/17/15.
+//  Copyright © 2015 Danny Dong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    
+    var data = ["Apple", "Apricot", "Banana", "Blueberry", "Cantaloupe", "Cherry",
+        "Clementine"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("FruitCell", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+}
+
